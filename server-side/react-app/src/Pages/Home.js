064325680bf0d7f8ab36b31e293b61c29946672e
Vedant Pandey <vedantpandey459@gmail.com>
2023-10-21 20:38:04 +0530

    home bettered

--- server-side/react-app/src/Pages/Home.js
+++ server-side/react-app/src/Pages/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
+import './pages.css';
+import { useNavigate } from "react-router-dom";
 export default function HomePage(){
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1>Welcome to Beanjam!!</h1>
+        <div className="HomePage">
+            <h1 style={{marginBottom:'50px'}}>Welcome to Beanjam!!</h1>
+            <div>
+            <button onClick={ () => { navigate("/login")}}>Login</button>
+            <button onClick={ () => { navigate("/signup")}}>Sign Up</button>
+            </div>
         </div>
     )
 }
